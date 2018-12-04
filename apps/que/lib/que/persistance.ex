4b2547b15b9bdbebed54cf9ba28ac2b4c1422bc4
Zheludkov Artur <a.zheludkov@ticketstravelnetwork.com>
2018-12-04 10:32:19 +0200

    create persistance delegate

---
+++ apps/que/lib/que/persistance.ex
@@ -0,0 +1,30 @@
+defmodule Que.Persistence do
+
+  #
+  # Here we may put adapter from config,
+  # either from environment variable of the node
+  # in future if needed
+  #
+  @adapter Application.get_env(:que, :persistance)
+
+  @doc """
+  Find all jobs from the db.
+  """
+  defdelegate get_all_jobs(), to: @adapter
+
+  @doc """
+  Inserts new task into db.
+  Returns new task record
+  """
+  defdelegate insert(term), to: @adapter
+
+  @doc """
+  Delete task into db.
+  """
+  defdelegate delete(task_id), to: @adapter
+
+  @doc """
+  Update task reason.
+  """
+  defdelegate update(task_id, reason), to: @adapter
+end
