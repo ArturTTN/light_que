fd2da2201c3cc876e69b5c43eba5d377f0ccbf55
Zheludkov Artur <a.zheludkov@ticketstravelnetwork.com>
2018-12-04 11:08:35 +0200

    add empty test for db

--- apps/pg/test/pg_test.exs
+++ apps/pg/test/pg_test.exs
@@ -2,7 +2,20 @@ defmodule PgTest do
   use ExUnit.Case
   doctest Pg
 
-  test "greets the world" do
-    assert Pg.hello() == :world
+  test "get_all_jobs" do
+    assert Pg.get_all_jobs() == []
   end
+
+  test "insert" do
+    assert Pg.insert(%{}) == :ok
+  end
+
+  test "delete" do
+    assert Pg.delete(1) == :ok
+  end
+
+  test "update" do
+    assert Pg.update(1, :success) == :ok
+  end
+
 end
